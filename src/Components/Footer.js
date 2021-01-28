9146b4453917dbba29d59f77158027bc9f00856f
Lillian Paris <lillian.paris529@gmail.com>
2021-01-27 20:40:42 -0700

    Added to the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
             <footer className="footer-bs">
                 <div className="row">
                     <div className="col-md-3 footer-brand animated fadeInLeft">
-                        <h2>Logo</h2>
+                        <h2>Lillian Paris</h2>
                         <p>Suspendisse hendrerit tellus laoreet luctus pharetra. Aliquam porttitor vitae orci nec ultricies. Curabitur vehicula, libero eget faucibus faucibus, purus erat eleifend enim, porta pellentesque ex mi ut sem.</p>
                         <p>© 2014 BS3 UI Kit, All rights reserved</p>
                     </div>
@@ -21,41 +21,43 @@ const Footer = () => {
                         <h4>Menu —</h4>
                         <div className="col-md-6">
                             <ul class="pages">
-                                <li><a href="#">Travel</a></li>
-                                <li><a href="#">Nature</a></li>
-                                <li><a href="#">Explores</a></li>
-                                <li><a href="#">Science</a></li>
-                                <li><a href="#">Advice</a></li>
+                                <li><a href="#">Workday Scheduler</a></li>
+                                <li><a href="#">Invitor +</a></li>
+                                <li><a href="#">Note Taker</a></li>
+                                <li><a href="#">Team Profile Generator</a></li>
+                                <li><a href="#">Movie Eaters</a></li>
+                                <li><a href="#">Password Generator</a></li>
                             </ul>
                         </div>
                         <div className="col-md-6">
                             <ul class="list">
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">Contacts</a></li>
-                                <li><a href="#">Terms & Condition</a></li>
-                                <li><a href="#">Privacy Policy</a></li>
+                                <li><a href="#">React-Bootsrap</a></li>
+                                <li><a href="#">JSX</a></li>
+                                <li><a href="#">React</a></li>
+                                <li><a href="#">SQL</a></li>
                             </ul>
                         </div>
                     </div>
                     <div className="col-md-2 footer-social animated fadeInDown">
-                        <h4>Follow Us</h4>
+                        <h4>Contact Me</h4>
                         <ul>
-                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Github</a></li>
+                            <li><a href="#">Resume</a></li>
+                            <li><a href="#">Email</a></li>
+                            <li><a href="#">Linkedln</a></li>
                             <li><a href="#">Twitter</a></li>
-                            <li><a href="#">Instagram</a></li>
-                            <li><a href="#">RSS</a></li>
                         </ul>
                     </div>
                     <div className="col-md-3 footer-ns animated fadeInRight">
-                        <h4>Newsletter</h4>
-                        <p>A rover wearing a fuzzy suit doesn’t alarm the real penguins</p>
+                        <h4>Thank You for Visiting</h4>
+                        <p></p>
                         <p>
-                            <div className="input-group">
+                            {/* <div className="input-group">
                                 <input type="text" class="form-control" placeholder="Search for..." />
                                 <span className="input-group-btn">
                                     <button className="btn btn-default" type="button"><span class="glyphicon glyphicon-envelope"></span></button>
                                 </span>
-                            </div>
+                            </div> */}
                         </p>
                     </div>
                 </div>
