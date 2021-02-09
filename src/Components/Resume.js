35e45695113a508742297743081d341b69120f58
Lillian Paris <lillian.paris529@gmail.com>
2021-02-08 23:16:00 -0700

    Disabled eslint for iframe

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,6 +6,7 @@ import PDF from './NewResume2.pdf';
 const Resume = () => {
     return (
         <>
+        // eslint-disable-next-line jsx-a11y/iframe-has-title
         <iframe src={PDF} height="1000" width="1300" ></iframe>
         </>
         );
